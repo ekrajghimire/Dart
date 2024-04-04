e946c8c9061a4704253538b98da9d7c04e06f924
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2024-04-04 11:23:55 +0530

    Add files via upload

---
+++ Function Parameter/2.dart
@@ -0,0 +1,13 @@
+// Write a program in Dart to print even numbers between intervals using function.
+
+void evenNo() {
+  for (int i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
+}
+
+void main(List<String> args) {
+  evenNo();
+}
